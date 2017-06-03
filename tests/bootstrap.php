a8b360159c7265e1b7c5c061c18cda34243602dd
Derrick Hammer <derrick@derrickhammer.com>
2017-06-02 20:14:35 -0400

    *Bootstrap global composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+if (file_exists(getenv('COMPOSER_HOME') . '/vendor/autoload.php')) {
+    require_once getenv('COMPOSER_HOME') . '/vendor/autoload.php';
+}
+
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
